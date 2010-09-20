ab53d1440034d7b97a74a5c39bad8adeec73378e
Caires Vinicius <cairesvs@gmail.com>
2010-09-20 13:49:59 -0300

    formfy js

---
+++ formfy.js
@@ -0,0 +1,103 @@
+function Formfy(){
+  var form = document.createElement("form");
+  this.with = {
+			attribute : function(name, value){
+				form.setAttribute(name, value);
+				return this
+			},
+      action : function(action){
+				this.attribute("action", action);
+				return this;
+      },
+			method : function(method){
+				this.attribute("method", method);
+				return this;
+			},
+			id : function(id){
+				this.attribute("id", id);
+				return this;
+			},
+			input: function(attributes){
+				var input = document.createElement("input");
+				for(var attribute in attributes){
+					input.setAttribute(attribute, attributes[attribute]);
+				}
+				form.appendChild(input);
+				return this;
+			},
+			inputs : function(){
+			  for (var i = 0; i < arguments.length; i++){
+					var arg = arguments[i];
+					this.input(arg);
+				}
+				return this;
+			},
+			text : function(name){
+				var skeleton = {
+					type: "text", name : name
+				};
+				return this.input(skeleton);
+			},
+			checkbox : function(name){
+				var skeleton = {
+					type: "checkbox", name : name
+				};
+				return this.input(skeleton);
+			},
+			button : function(value){
+				var skeleton = {
+					type: "button", value : value
+				};
+				return this.input(skeleton);
+			},
+			password : function(name){
+				var skeleton = {
+					type: "password", name : name
+				};
+				return this.input(skeleton);
+			},
+			radio : function(name, value){
+				var skeleton = {
+					type:"radio", name : name, value: value
+				};
+				return this.input(skeleton);
+			},
+			select : function(name, fn){
+				var selectfy = new Selectfy(name);
+				fn.call(selectfy, selectfy);
+				form.appendChild(selectfy.with.select());
+				return this;
+			},
+      done : function(){
+				return form;
+      }
+  }
+}
+
+function Selectfy(name){
+	var select = document.createElement('select');
+	select.name = name;
+	
+	this.with = {
+		options : function(){
+			for (var i = 0; i < arguments.length; i++){
+				var arg = arguments[i];
+				this.option(arg);
+			}
+			return this;
+		},
+		option : function(value,text){
+			var arg = typeof value == "string" ? { value :  value} : value;
+			var option = document.createElement('option');
+			option.appendChild(document.createTextNode(text));
+			for(var attribute in arg){
+				option.setAttribute(attribute, arg[attribute]);
+			}
+			select.appendChild(option);
+			return this;
+		},
+		select : function(){
+			return select;
+		}
+	}
+}
