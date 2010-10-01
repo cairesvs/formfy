f216a28a7c140d1e98c0b08c93210b05de49dc01
Caires Vinicius <cairesvs@gmail.com>
2010-10-01 17:41:36 -0300

    adicionando mais exemplos e refatorando o codigo

--- formfy.js
+++ formfy.js
@@ -1,76 +1,84 @@
 function Formfy(){
   var form = document.createElement("form");
   this.with = {
-			attribute : function(name, value){
-				form.setAttribute(name, value);
-				return this
-			},
-      action : function(action){
-				this.attribute("action", action);
-				return this;
-      },
-			method : function(method){
-				this.attribute("method", method);
-				return this;
-			},
-			id : function(id){
-				this.attribute("id", id);
-				return this;
-			},
-			input: function(attributes){
-				var input = document.createElement("input");
-				for(var attribute in attributes){
-					input.setAttribute(attribute, attributes[attribute]);
-				}
-				form.appendChild(input);
-				return this;
-			},
-			inputs : function(){
-			  for (var i = 0; i < arguments.length; i++){
-					var arg = arguments[i];
-					this.input(arg);
-				}
-				return this;
-			},
-			text : function(name){
-				var skeleton = {
-					type: "text", name : name
-				};
-				return this.input(skeleton);
-			},
-			checkbox : function(name){
-				var skeleton = {
-					type: "checkbox", name : name
-				};
-				return this.input(skeleton);
-			},
-			button : function(value){
-				var skeleton = {
-					type: "button", value : value
-				};
-				return this.input(skeleton);
-			},
-			password : function(name){
-				var skeleton = {
-					type: "password", name : name
-				};
-				return this.input(skeleton);
-			},
-			radio : function(name, value){
-				var skeleton = {
-					type:"radio", name : name, value: value
-				};
-				return this.input(skeleton);
-			},
-			select : function(name, fn){
-				var selectfy = new Selectfy(name);
-				fn.call(selectfy, selectfy);
-				form.appendChild(selectfy.with.select());
-				return this;
-			},
-      done : function(){
-				return form;
+    attribute : function(name, value){
+      form.setAttribute(name, value);
+      return this
+    },
+    action : function(action){
+      this.attribute("action", action);
+      return this;
+    },
+    method : function(method){
+      this.attribute("method", method);
+      return this;
+    },
+    get: function(){
+      this.attribute("method","get");
+      return this;
+    },
+    post: function(){
+      this.attribute("method","post");
+      return this;
+    },
+    id : function(id){
+      this.attribute("id", id);
+      return this;
+    },
+    input: function(attributes){
+      var input = document.createElement("input");
+      for(var attribute in attributes){
+        input.setAttribute(attribute, attributes[attribute]);
       }
+      form.appendChild(input);
+      return this;
+    },
+    inputs : function(){
+      for (var i = 0; i < arguments.length; i++){
+        var arg = arguments[i];
+        this.input(arg);
+      }
+      return this;
+    },
+    text : function(name){
+      var skeleton = {
+        type: "text", name : name
+      };
+      return this.input(skeleton);
+    },
+    checkbox : function(name){
+      var skeleton = {
+        type: "checkbox", name : name
+      };
+      return this.input(skeleton);
+    },
+    button : function(value){
+      var skeleton = {
+        type: "button", value : value
+      };
+      return this.input(skeleton);
+    },
+    password : function(name){
+      var skeleton = {
+        type: "password", name : name
+      };
+      return this.input(skeleton);
+    },
+    radio : function(name, value){
+      var skeleton = {
+        type:"radio", name : name, value: value
+      };
+      return this.input(skeleton);
+    },
+    select : function(name, fn){
+      var selectfy = new Selectfy(name);
+      fn.call(selectfy, selectfy);
+      form.appendChild(selectfy.with.select());
+      return this;
+    },
+    done : function(){
+      return form;
+    }
   }
 }
 
@@ -80,9 +88,9 @@ function Selectfy(name){
 	
 	this.with = {
 		options : function(){
-      console.log(arguments)
 			for (var i = 0; i < arguments.length; i++){
 				var arg = arguments[i];
+        if(!arg['text']) throw ReferenceError("Text for option is not defined");
 				this.option(arg, arg['text']);
 			}
 			return this;
@@ -90,6 +98,7 @@ function Selectfy(name){
 		option : function(value, text){
 			var arg = typeof value == "string" ? { value :  value} : value;
 			var option = document.createElement('option');
+      if(!text) throw ReferenceError("Text for option is not defined");
       option.appendChild(document.createTextNode(text));
 			for(var attribute in arg){
 				option.setAttribute(attribute, arg[attribute]);
