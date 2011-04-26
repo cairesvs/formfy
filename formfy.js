8c1087c61c2033298d7cff9e7c2c14137a266e0a
Cairê Vinicius <cairesvs@gmail.com>
2011-04-26 14:04:41 -0700

    reverting...

--- formfy.js
+++ formfy.js
@@ -1,6 +1,6 @@
 function Formfy(){
   var form = document.createElement("form");
- // form.style.display = 'none';
+  form.style.display = 'none';
 
   this.with = {
     attribute : function(name, value){
@@ -35,7 +35,7 @@ function Formfy(){
         value : "put"
       });
     },
-    _delete : function(){
+    delete : function(){
      return this.post().input({
         type : "_method",
         value : "delete"
@@ -130,38 +130,4 @@ function Selectfy(name){
 			return select;
 		}
 	}
-}
-
-function Objectfy(){
-  var fields = [];
-  var fieldOptions = [];
-
-  this.has = function(name){
-    fields.push({
-      type : 'text',
-      name : name,
-      id : name
-    });
-  }
-
-  this.is = function(name){
-    fields.push({
-      type : 'checkbox',
-      name : name,
-      id : name
-    });
-  }
-
-  this.canChoose = function(option){
-    new Selectfy(
-  }
-
-  this.for = function(name){
-    
-  }
-
-  this.done = function(){
-    return fields;
-  }
-
 }
